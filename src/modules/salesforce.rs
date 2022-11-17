3636c3fb0742c42377310510a3f1b94a198eb9ab
Przemek <rawa.przemek@gmail.com>
2022-11-17 22:03:33 +0100

    org discovery using command
    in the future, we will have to detect it from files because using command is very slow

--- src/modules/salesforce.rs
+++ src/modules/salesforce.rs
@@ -1,18 +1,63 @@
 use crate::config::ModuleConfig;
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
 use crate::configs::salesforce::SalesforceConfig;
-use crate::formatter::{StringFormatter, VersionFormatter};
+use crate::formatter::{StringFormatter};
 use super::{Context, Module};
+
+
+#[derive(Clone, Deserialize, Serialize)]
+#[serde(default)]
+struct SfdxOrgCommandOutputResult {
+    pub alias: Option<String>,
+    pub username: Option<String>,
+}
+
+impl Default for SfdxOrgCommandOutputResult {
+    fn default() -> Self {
+        SfdxOrgCommandOutputResult { alias: None, username: None }
+    }
+}
+
+#[derive(Clone, Deserialize, Serialize)]
+#[serde(default)]
+struct SfdxOrgCommandOutput {
+    pub status: i64,
+    pub result: Option<SfdxOrgCommandOutputResult>,
+}
+
+impl Default for SfdxOrgCommandOutput {
+    fn default() -> Self {
+        SfdxOrgCommandOutput { status: 1, result: None }
+    }
+}
 
 pub fn module<'a>(context: &'a Context) -> Option<Module<'a>> {
     let mut module = context.new_module("salesforce");
     let config = SalesforceConfig::try_load(module.config);
-    // context.dir_contents().ok()?.has_file_name()
+    let output = context.exec_cmd("sfdx", &["force:org:display", "--json"])?.stdout;
+    let parsed: Value = serde_json::from_str(output.as_str()).ok()?;
+
+
+    let org_name = match parsed.get("result") {
+        Some(result) => {
+            match  result.get("alias"){
+                Some(alias) => alias.as_str().unwrap(),
+                None => {
+                    match result.get("username") {
+                        Some(username) => username.as_str().unwrap(),
+                        None => return None
+                    }
+                }
+            }
+        },
+        None => return None
+    };
 
     let parsed = StringFormatter::new(config.format).and_then(|formatter| {
         formatter
             .map_meta(|var, _| match var {
                 "symbol" => Some(config.symbol),
-
                 _ => None,
             })
             .map_style(|variable| match variable {
@@ -21,7 +66,7 @@ pub fn module<'a>(context: &'a Context) -> Option<Module<'a>> {
             })
             .map(|variable| match variable {
                 "org_name" => {
-                    return Some(Ok("Adasd"));
+                    return Some(Ok(org_name.clone()));
                 }
                 _ => None,
             })
@@ -31,10 +76,9 @@ pub fn module<'a>(context: &'a Context) -> Option<Module<'a>> {
         Ok(segments) => segments,
         Err(error) => {
             log::warn!("Error in module 'salesforce':\n{}", error);
-            return None
+            return None;
         }
     });
-
 
     Some(module)
 }
